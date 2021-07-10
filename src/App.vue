d6f129550b90686fb16800169c4e80d3f0a8c3f1
Emre Demir <pi.emree@gmail.com>
2021-07-10 13:07:15 +0300

    test30

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1 class="title">Math Quiz App </h1>
+    <h1 class="title">Math Quiz App test </h1>
 
     <OperatorQuiz @onBack="clearOperator" v-if="operator" :operator="operator" />
     <OperatorSelector v-else @changeOperator="changeOperator" />
