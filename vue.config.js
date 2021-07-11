e0ac3fd321e96af7103de5aed122424610964a19
Emre Demir <pi.emree@gmail.com>
2021-07-11 14:24:36 +0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    pwa: {
+        name: 'My App',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    
+        // configure the workbox plugin
+    
+      },
+
     publicPath: process.env.NODE_ENV === 'production'
       ? '/basic-quiz-app/'
       : '/'
